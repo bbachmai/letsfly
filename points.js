80eebfb058c8b92f245d2ab2650933516ceb551b
bbachmai <42718632+bbachmai@users.noreply.github.com>
2020-06-18 22:39:09 +0200

    Uploaded all basic website elements

---
+++ points.js
@@ -0,0 +1,81 @@
+knausr_pts_callback( { "type": "FeatureCollection", 
+	"features": [
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [11.5662, 47.7576]},
+		"properties": {"name": "Bad Toelz"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [11.1666, 47.3475]},
+		"properties": {"name": "Seefeld"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [10.8265, 47.3193]},
+		"properties": {"name": "Nassereith"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [10.4767, 47.1662]},
+		"properties": {"name": "Parseierspitze"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [9.9151, 47.0718]},
+		"properties": {"name": "Montafon"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [8.8728, 46.8731]},
+		"properties": {"name": "Klausenpass"}
+	},
+	{ 	"type": "Feature",
+		"id": "Achensee",
+		"geometry": {"type": "Point", "coordinates": [11.7635, 47.4185]},
+		"properties": {"name": "Achensee"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [11.9981, 47.2137]},
+		"properties": {"name": "Gerlos"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [13.0951, 47.3755]},
+		"properties": {"name": "Hochkoenig"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [12.5241, 47.5298]},
+		"properties": {"name": "Kirchbergstock"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [14.0151, 47.5195]},
+		"properties": {"name": "Grimming"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [12.1633, 47.5926]},
+		"properties": {"name": "Kufstein"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [5.1119, 44.2303]},
+		"properties": {"name": "Orange (Rhone)"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [5.8850, 44.3955]},
+		"properties": {"name": "Col de Faye"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [6.0333, 44.3333]},
+		"properties": {"name": "La Motte Du Caire"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [6.4116, 44.1167]},
+		"properties": {"name": "Cheval Blanc"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [6.4781, 44.2667]},
+		"properties": {"name": "Les Trois Eveches"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [6.4250, 44.5756]},
+		"properties": {"name": "Mont Guillaume"}
+	},
+	{ 	"type": "Feature",
+		"geometry": {"type": "Point", "coordinates": [6.6166, 44.7000]},
+		"properties": {"name": "St. Crepin"}
+	}
+ 	]
+})
---
+++ script.js
@@ -0,0 +1,63 @@
+var map;
+var popup_closed = 1;
+function createMap () {
+	var options = {
+		center: { lat: 46.5, lng: 8.47 },
+		zoom: 6,
+		mapTypeId: google.maps.MapTypeId.TERRAIN
+	};
+	map = new google.maps.Map(document.getElementById('map'), options);
+	
+	var points_script = document.createElement('script');
+	points_script.src = 'points.js';
+	document.getElementsByTagName('head')[0].appendChild(points_script);
+	
+	var therm_on = document.getElementById('therm');
+	var wave_on = document.getElementById('wave');
+	
+	if (therm_on.checked == true) {
+		var routes_script = document.createElement('script');
+		routes_script.src = 'routes_therm.js';
+		document.getElementsByTagName('head')[0].appendChild(routes_script);
+	}
+	
+	if (wave_on.checked == true) {
+		var routes_wave_script = document.createElement('script');
+		routes_wave_script.src = 'routes_wave.js';
+		document.getElementsByTagName('head')[0].appendChild(routes_wave_script);
+	}
+	
+	map.data.setStyle({
+	icon: 'points_icon.png',
+	strokeWeight: 7.0,
+	clickable: true
+	});
+	
+	map.data.addListener('mouseover',function(event) {
+	if (popup_closed == 1) {
+		var popup = new google.maps.InfoWindow({
+			content:
+			event.feature.getProperty("name"),
+			position: event.latLng,
+			pixelOffset: new google.maps.Size(-5,-5)
+		});
+		popup.open(map);
+		popup_closed = 0;	
+	}
+	setTimeout(function(){popup.close();popup_closed = 1;},'800');
+	});
+	
+	map.data.addListener('click',function(event) {
+		if (event.feature.getProperty("address")) {
+		window.open(event.feature.getProperty("address"), '_blank');
+		};
+	});
+}
+	
+function knausr_pts_callback (geojson) {
+	map.data.addGeoJson(geojson);
+}	
+
+function knausr_routes_callback (geojson) {
+	map.data.addGeoJson(geojson);
+}	
